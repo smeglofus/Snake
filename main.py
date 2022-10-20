39a930a52b347a0a5c8c1e1983607393fc087d20
smeglofus <smeglofus@gmail.com>
2022-10-20 15:47:13 +0200

    refractoring

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
 import time
 import pygame
 from random import randrange
-import datetime
 from player_name import *
 from scoreboard import Scoreboard, save_score
-import json
 
 pygame.init()
 
 clock = pygame.time.Clock()
+WEIGHT_OF_FOOD = [10, 10]
 SNAKE_LIST = []
 LENGHT_OF_SNAKE = 1
 SNAKE_SPEED = 25
 DYSPLAY_WIDTH = 800
 DYSPLAY_HEIGHT = 600
-font_style = pygame.font.SysFont(None,35)
+font_style = pygame.font.SysFont(None, 35)
 dysplay = pygame.display.set_mode((DYSPLAY_WIDTH, DYSPLAY_HEIGHT))
 pygame.display.set_caption("Smeglofův had")
 game_over = False
 
-#souradnice hada
+# ouradnice hada
 x1 = DYSPLAY_WIDTH / 2
 y1 = DYSPLAY_HEIGHT / 2
 x1_change = 0
 y1_change = 0
-#souradnice jídla
-foodx = randrange(0, DYSPLAY_WIDTH - 20)
-foody = randrange(0, DYSPLAY_HEIGHT - 20)
+FOODX = randrange(0, DYSPLAY_WIDTH - 20)
+FOODY = randrange(0, DYSPLAY_HEIGHT - 20)
 
+# ----------------------------------------------------------------------Block of functions
 
-#-----------------------------------------------Block of functions
-def food_random_cor():
-    global foodx,foody
-    foodx = randrange(0, DYSPLAY_WIDTH - 20)
-    foody = randrange(0, DYSPLAY_HEIGHT - 20)
 def message(msg,color):
     mesg = font_style.render(msg, True, color)
     dysplay.blit(mesg, [DYSPLAY_WIDTH / 2, DYSPLAY_HEIGHT / 2])
 def make_had(snake_list):
     for cor in snake_list:
-        pygame.draw.rect(dysplay, "green", [cor[0],cor[1], 10, 10])
+        pygame.draw.rect(dysplay, "green", [cor[0], cor[1], 10, 10])
 def write_score(lenght_of_snake):
     score = font_style.render(f"Score: {lenght_of_snake-1}", True, "white")
     dysplay.blit(score, [20,20])
@@ -50,21 +44,23 @@ def show_scoreboard():
     scoreboard = Scoreboard()
     scoreboard.mainloop()
 
-#pokud se had dostane za hranice okna, nastává konec hry
+# pokud se had dostane za hranice okna, nastává konec hry
 def colision_with_wall():
     global  game_over
     if x1 >= DYSPLAY_WIDTH or x1 < 0 or y1 >= DYSPLAY_HEIGHT or y1 < 0:
         save_score(LENGHT_OF_SNAKE,message)
         game_over = True
-def eat_and_grow(foodx,foody):
-    global LENGHT_OF_SNAKE
-    if x1 in range(foodx-10, foodx+10) and y1 in range(foody-10, foody+10):
-        food_random_cor()
+def eat_and_grow(x,y):
+    global LENGHT_OF_SNAKE,FOODX,FOODY
+    pygame.draw.rect(dysplay, "red", [x, y, 10, 10])
+    if x1 in range(x-10, x+10) and y1 in range(y-10, y+10):
+        FOODX = randrange(0, DYSPLAY_WIDTH - 20)
+        FOODY = randrange(0, DYSPLAY_HEIGHT - 20)
         LENGHT_OF_SNAKE += 1
-    pygame.draw.rect(dysplay, "red", [foodx, foody, 10, 10])
     grow()
 def move():
-    global x1, y1, x1_change, y1_change
+    # souradnice hada
+    global x1,y1,x1_change,y1_change
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
@@ -93,13 +89,13 @@ def grow():
     if len(SNAKE_LIST) > LENGHT_OF_SNAKE:
         del SNAKE_LIST[0]
     make_had(SNAKE_LIST)
-#---------------------------------------------------
+# ------------------------------------------------------------------------------------
 
+#ask_name()
 
-ask_name()
 while not game_over:
     move()
-    eat_and_grow(foodx,foody)
+    eat_and_grow(FOODX,FOODY)
     write_score(LENGHT_OF_SNAKE)
     pygame.display.update()
     clock.tick(SNAKE_SPEED)
@@ -108,7 +104,7 @@ message("Konec hry","red")
 pygame.display.update()
 time.sleep(1)
 
-show_scoreboard()
+#show_scoreboard()
 
 pygame.quit()
 quit()
